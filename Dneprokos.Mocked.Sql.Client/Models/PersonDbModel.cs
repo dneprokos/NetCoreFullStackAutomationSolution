d55c2a949952614aa27b5605da6fce9dd57d7085
Kostiantyn Teltov <dneprokos@gmail.com>
2024-01-16 15:23:45 +0100

    Added SQL tests prototypes

---
+++ Dneprokos.Mocked.Sql.Client/Models/PersonDbModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Dneprokos.Mocked.Sql.Client.Models
+{
+    public class PersonDbModel
+    {
+        public int? Id { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public bool? IsActive { get; set; }
+    }
+}
---
+++ Dneprokos.Mocked.Sql.Client/SqlScripts/PersonTableSqlScripts.cs
@@ -0,0 +1,19 @@
+﻿namespace Dneprokos.Mocked.Sql.Client.SqlScripts
+{
+    public class PersonTableSqlScripts
+    {
+        public const string CreateTableScript = @"
+                CREATE TABLE Person
+                (
+                    Id INT IDENTITY(1,1) PRIMARY KEY,
+                    FirstName NVARCHAR(100),
+                    LastName NVARCHAR(100),
+                    IsActive BIT
+                );";
+
+        public const string InsertDefaultValuesScript = @"
+                INSERT INTO Person (FirstName, LastName, IsActive)
+                VALUES ('John', 'Doe', 1),
+                ('Jane', 'Doe', 0);";
+    }
+}
