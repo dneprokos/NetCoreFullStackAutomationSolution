b8d5394ea01a2e20d9e565a581a622afe6a59de7
Kostiantyn Teltov <dneprokos@gmail.com>
2024-01-18 15:08:10 +0100

    Refactored API Client implementation

---
+++ Dneprokos.Api.Base.Client/Authorizations/BasicAuthorization.cs
@@ -0,0 +1,22 @@
+﻿namespace Dneprokos.Api.Base.Client.Authorizations
+{
+    public class BasicAuthorization
+    {
+        public string? UserName { get; set; }
+
+        public string? Password { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public BasicAuthorization()
+        {
+        }
+
+        public BasicAuthorization(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+    }
+}
