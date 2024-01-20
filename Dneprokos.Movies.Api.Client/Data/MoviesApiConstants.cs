63831356e31f46aeb99480f835d23b19399d4d54
Kostiantyn Teltov <dneprokos@gmail.com>
2024-01-20 10:03:17 +0100

    Search Genres API test examples were added

--- Dneprokos.Movies.Api.Client/Data/MoviesApiConstants.cs
+++ Dneprokos.Movies.Api.Client/Data/MoviesApiConstants.cs
@@ -2,8 +2,16 @@
 {
     public class MoviesApiConstants
     {
+        public const int DefautPageNumber = 1;
+
+        public const int DefautPageLimit = 10;
+
         public const string NoUserPermissionsMessage = "Your user role cannot perform this operation";
 
         public const string NameLessSymbolsMesssage = "\"name\" length must be at least 3 characters long";
+
+        public const string PageLessThanMinMessage = "\"page\" must be larger than or equal to 1";
+
+        public const string LimitLessThanMinMessage = "\"limit\" must be larger than or equal to 1";
     }
 }
